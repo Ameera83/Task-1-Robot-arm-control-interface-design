d8d285622ef020ff91c419fd0c3bd348363ebe26
Ameera83 <86646766+Ameera83@users.noreply.github.com>
2021-07-01 06:10:39 +0300

    Add files via upload

--- task1.php
+++ task1.php
@@ -3,13 +3,22 @@
   <?php
 $servername = "localhost";
 $username = "root";
-$password = "password";
+$password = "";
 $dbname = "db_1";
 
-// Create connection
-$con = mysqli_connect('servername', 'username', 'password', 'dbname');
 
-$con = mysqli_connect('localhost','root','', 'db_1');
+
+
+//create connection
+$conn = new mysqli("localhost", "root", "", "db_1");
+if(mysqli_connect_error()){
+
+die();
+
+}
+
+else
+if(isset($_POST['save'])){
 
 
 $motor1 = $_POST['motor1:'];
@@ -18,19 +27,38 @@ $motor3 = $_POST['motor3:'];
 $motor4 = $_POST['motor4:'];
 $motor5 = $_POST['motor5:'];
 $motor6 = $_POST['motor6:'];
-//database insert SQL code
 
-$sql = "INSERT INTO motors_1 (motor1, motor2, motor3, motor4, motor5,motor6)
-VALUES ('motor1', 'motor2' , 'motor3', 'motor4', 'motor5', 'motor6')";
 
-//insert in database
-$rs = mysqli_query($con, $sql);
-if($rs)
+$sql =  "INSERT INTO motors (Motor1, Motor2, Motor3, Motor4, Motor5, Motor6)
+VALUES('$motor1', '$motor2' , '$motor3', '$motor4', '$motor5', '$motor6')";
+
+if($conn->query($sql)){
+
+    echo"The data inserted successfully";
+
+}
+
+else{
+
+    die();
+}
+}
+if(isset($_POST['run'])){
+
+
+
+$sql = "select * from motors_1 ";
+
+if($conn->query( $sql))
 {
-    echo"Value Added Successfully";
 
+    echo "Item added successfully!";
+}
+else{
 
-
+    die();
+}
+mysql_close($conn);
 }
 ?>
 
